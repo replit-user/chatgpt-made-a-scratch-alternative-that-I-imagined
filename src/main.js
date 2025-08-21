6f884778bb275d28a8d0c0d81324940a41b3cfdb
replit-user <umyashinderu@gmail.com>
2025-08-21 15:07:05 -0600

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import VPLEditor from "./VPLEditor";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <VPLEditor />
+  </React.StrictMode>
+);
